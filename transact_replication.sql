c99673c75a9594cbb2b18475cd56eacb788dd160
Mariusz Masewicz <mmasel@gmail.com>
2021-03-16 16:10:58 +0100

    z

--- transact_replication.sql
+++ transact_replication.sql
@@ -1,6 +1,10 @@
 use distribution
 go
 
+select * from dbo.MSrepl_errors
+where error_code in ('2601','2627','20598')
+order by time desc
+
 exec sp_browsereplcmds 
 @xact_seqno_start = '0x000456FA00103618002E00000000', 
 @xact_seqno_end = '0x000456FA00103618002E00000000'
